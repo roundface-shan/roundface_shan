c833d47ddc0eaf16f8e0abe274a69c95034718c3
roundface-shan <lice2567614@gmail.com>
2023-03-20 18:41:06 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'roundface_shan/master'.
    
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   rollup/rollup.config.js
    	new file:   rollup/rollup.esm.config.js
    	modified:   src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-
+import './style/index.scss'
 library.add(fas)
 
 export { default as Button } from './components/Button'
